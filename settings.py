647544bd4e608fe8810c6e75e4462ab1011534dc
Dieplex <glebsergee@mail.ru>
2022-05-02 02:03:57 +0300

    Add Docker

--- settings.py
+++ settings.py
@@ -4,3 +4,6 @@ token = os.environ.get('HD_TOKEN')
 msg_token = os.environ.get('HD_MSG_TOKEN')
 twin_token = os.environ.get('HD_TWIN_TOKEN')
 main_token = os.environ.get('HD_MAIN_TOKEN')
+confirmation_token = os.environ.get('HD_CONFIRMATION_TOKEN')
+group_id = os.environ.get('HD_GROUP_ID')
+
