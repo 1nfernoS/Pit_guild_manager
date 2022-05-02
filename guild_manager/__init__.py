4d52626f0397f2d057e444b68e2b41f1ddad9fdc
1nfernoS <gaydenko1801@gmail.com>
2022-05-02 03:17:53 +0300

    Added foreign checks

--- guild_manager/__init__.py
+++ guild_manager/__init__.py
@@ -1,5 +1,7 @@
 from traceback import format_exc
 from json import JSONDecodeError
+
+import requests
 from flask import Flask, request, json, make_response
 
 import settings
@@ -27,12 +29,13 @@ def handler():
     # confirmation don't send any other data
     try:
         type_msg = data['type']
+        group_id = data['group_id']
     except KeyError:
         return make_response("Wrong data provided", 400)
     except TypeError:
         return make_response("Wrong data provided", 400)
 
-    if type_msg == 'confirmation':
+    if type_msg == 'confirmation' and group_id == settings.group_id:
         return settings.confirmation_token
 
     try:
@@ -46,9 +49,20 @@ def handler():
 
     if type_msg == 'message_new':
         data_msg = obj_msg['message']
+        check()
         # message(data_msg)
 
     return make_response('ok', 200)
+
+
+@app.route('/check/')
+def check():
+    try:
+        r = requests.get('https://vip3.activeusers.ru/app.php?act=user&auth_key=5153d58b92d71bda47f1dac05afc187a&viewer_id=158154503&group_id=182985865&api_id=7055214')
+    except:
+        return '<h2>No Access to profile</h2><h4>' + format_exc(-2) + '</h4>'
+    if r.status_code == 200:
+        return '<h2>Access granted</h2>'
 
 
 @app.errorhandler(500)
