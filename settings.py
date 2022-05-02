e03c868cccae0826b15e79a3827212f7759bffe9
1nfernoS <gaydenko1801@gmail.com>
2022-05-02 03:22:52 +0300

    Added foreign checks

--- settings.py
+++ settings.py
@@ -1,9 +1,6 @@
 import os
 
-token = os.environ.get('HD_TOKEN')
-msg_token = os.environ.get('HD_MSG_TOKEN')
-twin_token = os.environ.get('HD_TWIN_TOKEN')
-main_token = os.environ.get('HD_MAIN_TOKEN')
+group_token = os.environ.get('HD_GROUP_TOKEN')
 confirmation_token = os.environ.get('HD_CONFIRMATION_TOKEN')
 group_id = os.environ.get('HD_GROUP_ID')
 
