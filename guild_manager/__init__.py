37759cca631c4950b3ae5d4bff0d468072ea7ee7
1nfernoS <gaydenko1801@gmail.com>
2022-05-04 10:31:55 +0300

    Added TODO

--- guild_manager/__init__.py
+++ guild_manager/__init__.py
@@ -102,6 +102,15 @@ def message(msg):
     text = str(msg['text'])
     chat = int(msg['peer_id'])
     user = int(msg['from_id'])
+    # TODO:
+    #  1. Initialize item list (DB or smth)
+    #  2. Check logs (Overseer, gold, shatters)
+    #  3. DB for users (id, url profile, etc) and buffers (id, type, token, chat_id, etc)
+    #  4. Pricing items?
+    #  5. CM functions (kick, punishments, announcements
+    #  6. Notes and stuff
+    #  7. In-game assist (books, walk, questions, road cross, etc)
+    '''
     if text.startswith('/'):
         if user == settings.creator_id:
             if text == '/check':
@@ -117,6 +126,7 @@ def message(msg):
                                   peer_id=str(chat),
                                   random_id=0,
                                   message=str(msg))
+    '''
     return
 
 
